7cd400cc27aeafaf96f90a26e4f8c932620c5da3
ilyogri <kokis24@abv.bg>
2017-06-10 00:53:49 +0300

    Edited Code

--- Methods and Debugging - Lab/07. Greater of Two Values/Program.cs
+++ Methods and Debugging - Lab/07. Greater of Two Values/Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _6.Math_Power
+namespace _7.Greater_of_Two_Values
 {
-    class Program
+    ﻿using System;
+    
+   public class Program
     {
-        static void Main()
+       public static void Main()
         {
             var type = Console.ReadLine().ToLower();
 
